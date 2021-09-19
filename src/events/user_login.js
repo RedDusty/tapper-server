34555c198205e52de481556a0fb5a9506b4302e2
RedDusty <zprobius@gmail.com>
2021-09-19 16:01:17 +0300

    ping, online - info

--- src/events/user_login.js
+++ src/events/user_login.js
@@ -1,18 +1,19 @@
 const { Socket } = require('socket.io');
 const db = require('../db').db;
 
-module.exports = function (/** @type {Socket} socket*/ socket) {
+module.exports = function (/** @type {Socket} socket*/ socket, io) {
   socket.on('USER_LOGIN', ({ nickname, avatar, skin, rank, firstLogin, uid, id }) => {
-    socket.join('users');
-
     if (id) {
       if (db.get('users').findIndex((el) => el.id === socket.id) === -1) {
         db.set('users', [
           ...db.get('users'),
           { nickname: nickname, avatar: avatar, skin: skin, rank: rank, firstLogin: firstLogin, uid: uid, id: id }
         ]);
-        socket.in('users').emit('USERS_UPDATE', { usersCount: db.get('users').length });
       }
     }
+
+    socket.join('users');
+    io.in('users').emit('USERS_UPDATE', { usersCount: db.get('users').length });
+    io.in('users').emit('PLAYING_UPDATE', { playingCount: db.get('playing').length });
   });
 };
--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ app.set('port', process.env.PORT || 3000);
 const http = require('http').Server(app);
 
 /** @type {socketio} */
-const io = require('socket.io')(http);
+const io = require('socket.io')(http, { pingInterval: 5000 });
 
 const db = require('./db').db;
 
@@ -17,7 +17,7 @@ io.on('connection', (/** @type {socketio.Socket} socket*/ socket) => {
   console.log(socket.id + ' is connected');
   socket.send(socket.id);
 
-  user_login(socket);
+  user_login(socket, io);
 
   socket.on('LOBBY_CREATE', (data) => {
     socket.leave('users');
@@ -35,10 +35,11 @@ io.on('connection', (/** @type {socketio.Socket} socket*/ socket) => {
       }
     ];
     db.set('lobby', newDBlobby);
-    socket.in('free').emit('LOBBY_UPDATE', db.get('games'));
+    socket.in('users').emit('LOBBY_UPDATE', db.get('games'));
 
     const newDBplaying = [
-      ...db.get('playing', {
+      ...db.get('playing'),
+      {
         nickname,
         avatar,
         skin,
@@ -46,22 +47,30 @@ io.on('connection', (/** @type {socketio.Socket} socket*/ socket) => {
         firstLogin,
         uid,
         id
-      })
+      }
     ];
     db.set('playing', newDBplaying);
 
     socket.in('users').emit('USERS_UPDATE', { usersCount: db.get('users').length });
+    socket.in('users').emit('PLAYING_UPDATE', { playingCount: db.get('playing').length });
 
     console.log(`Game ${data.id} has been created.`);
   });
 
   socket.on('disconnect', (reason) => {
-    socket.leave('users')
+    socket.leave('users');
     const index = db.get('users').findIndex((el) => el.id === socket.id);
-    const newArr = db.get('users').slice()
-    newArr.splice(index, 1)
+    const newArr = db.get('users').slice();
+    newArr.splice(index, 1);
     db.set('users', newArr);
     console.log(socket.id + ' has disconnected, reason: ' + reason);
+    socket.in('users').emit('USERS_UPDATE', { usersCount: db.get('users').length });
+  });
+
+  socket.on('ping', (func) => {
+    if (typeof func === 'function') {
+      func();
+    }
   });
 });
 
