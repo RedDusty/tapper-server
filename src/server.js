f5a002adad13aa855e3fbf6e1e24945d8912b085
RedDusty <zprobius@gmail.com>
2021-11-05 13:49:59 +0300

    cors options

--- src/server.js
+++ src/server.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const socketio = require("socket.io");
+require("dotenv").config();
 
 const cors = require("cors");
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.DEPLOY_APP,
+    credentials: true,
+    optionsSuccessStatus: 200,
+    methods: ["GET"],
+  })
+);
 app.set("port", PORT);
 
 const http = require("http").Server(app);
