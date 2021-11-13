a8b4d84146472a5bcdcf170613ed4fbece03baf5
RedDusty <zprobius@gmail.com>
2021-11-13 18:25:17 +0300

    lobby chat - messages fix // messages on change options

--- src/events/lobby/lobby_options.js
+++ src/events/lobby/lobby_options.js
@@ -1,51 +1,77 @@
-const { Socket } = require('socket.io');
-const { dbLobby } = require('../../db');
-const { getFreeLobbies } = require('../../functions');
+const { Socket } = require("socket.io");
+const { dbLobby } = require("../../db");
+const { getFreeLobbies } = require("../../functions");
 
 module.exports = function (/** @type {Socket} */ socket, io) {
-  socket.on('LOBBY_OPTIONS', (data) => {
+  socket.on("LOBBY_OPTIONS", (data) => {
     if (dbLobby.has(data.code)) {
+      const lobby = dbLobby.get(data.code);
       switch (data.option) {
-        case 'setVisibility': {
-          dbLobby.get(data.code).visibility = data.visibility;
-          emitter(io, data.code, data.visibility, 'setVisibility');
+        case "setVisibility": {
+          emitterMessage(
+            io,
+            data,
+            lobby.visibility,
+            data.visibility,
+            "Visibility"
+          );
+          lobby.visibility = data.visibility;
+          emitterOption(io, data.code, data.visibility, "setVisibility");
           break;
         }
-        case 'setShape': {
-          dbLobby.get(data.code).shape = data.shape;
-          emitter(io, data.code, data.shape, 'setShape');
+        case "setFieldX": {
+          emitterMessage(io, data, lobby.fieldX, data.fieldX, "Field X");
+          const emittedNumber = () => {
+            if (data.fieldX && Number(data.fieldX) > 0) {
+              return data.fieldX;
+            } else {
+              return String(1);
+            }
+          };
+          lobby.fieldX = emittedNumber();
+          emitterOption(io, data.code, data.fieldX, "setFieldX");
           break;
         }
-        case 'setRounds': {
-          dbLobby.get(data.code).rounds = data.rounds;
-          emitter(io, data.code, data.rounds, 'setRounds');
+        case "setFieldY": {
+          emitterMessage(io, data, lobby.fieldY, data.fieldY, "Field Y");
+          const emittedNumber = () => {
+            if (data.fieldY && Number(data.fieldY) > 0) {
+              return data.fieldY;
+            } else {
+              return String(1);
+            }
+          };
+          lobby.fieldY = emittedNumber();
+          emitterOption(io, data.code, data.fieldY, "setFieldY");
           break;
         }
-        case 'setFieldX': {
-          dbLobby.get(data.code).fieldX = data.fieldX;
-          emitter(io, data.code, data.fieldX, 'setFieldX');
+        case "setInLobbyPlayers": {
+          lobby.inLobbyPlayers = data.inLobbyPlayers;
+          emitterOption(
+            io,
+            data.code,
+            data.inLobbyPlayers,
+            "setInLobbyPlayers"
+          );
           break;
         }
-        case 'setFieldY': {
-          dbLobby.get(data.code).fieldY = data.fieldY;
-          emitter(io, data.code, data.fieldY, 'setFieldY');
-          break;
-        }
-        case 'setInLobbyPlayers': {
-          dbLobby.get(data.code).inLobbyPlayers = data.inLobbyPlayers;
-          emitter(io, data.code, data.inLobbyPlayers, 'setInLobbyPlayers');
-          break;
-        }
-        case 'setMaxPlayers': {
-          dbLobby.get(data.code).maxPlayers = data.maxPlayers;
-          emitter(io, data.code, data.maxPlayers, 'setMaxPlayers');
+        case "setMaxPlayers": {
+          emitterMessage(
+            io,
+            data,
+            lobby.maxPlayers,
+            data.maxPlayers,
+            "Max Players"
+          );
+          lobby.maxPlayers = data.maxPlayers;
+          emitterOption(io, data.code, data.maxPlayers, "setMaxPlayers");
           break;
         }
         // case 'userKick': {
-        //   const users = dbLobby.get(data.code).users;
+        //   const users = lobby.users;
         //   const newUsers = users.filter((u, i) => data.kickID !== u.id);
-        //   dbLobby.get(data.code).users = newUsers;
-        //   emitter(io, data.code, data.kickID, 'userKick');
+        //   lobby.users = newUsers;
+        //   emitterOption(io, data.code, data.kickID, 'userKick');
         //   break;
         // }
         default:
@@ -55,13 +81,30 @@ module.exports = function (/** @type {Socket} */ socket, io) {
   });
 };
 
-function emitter(io, code, option, type) {
-  const lobbyListArray = getFreeLobbies();
+function emitterMessage(io, data, oldValue, newValue, field) {
+  if (oldValue !== newValue) {
+    const lobby = dbLobby.get(data.code);
+    const messages = lobby.messages;
+    const messageData = {
+      avatar: "system",
+      code: data.code,
+      id: "system",
+      message: `${field}: ${oldValue} => ${newValue}`,
+      nickname: "System",
+      time: Date.now(),
+      uid: "system",
+    };
+    messages.push(messageData);
+    io.in(`LOBBY_${data.code}`).emit("LOBBY_GET_MESSAGES", lobby.messages);
+  }
+}
 
-  io.in('users').emit('LOBBY_GET', lobbyListArray);
-  io.in(`LOBBY_${code}`).emit('LOBBY_OPTIONS_UPDATE', {
+function emitterOption(io, code, option, type) {
+  const lobbyListArray = getFreeLobbies();
+  io.in("users").emit("LOBBY_GET", lobbyListArray);
+  io.in(`LOBBY_${code}`).emit("LOBBY_OPTIONS_UPDATE", {
     option,
     type,
-    code
+    code,
   });
 }
